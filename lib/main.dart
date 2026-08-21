27e2848f57f74479af450b9fc3b7b51e11dcbf76
adam741 <adam49863@gmail.com>
2026-08-21 22:21:12 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ import 'web_title_switcher_stub.dart' if (dart.library.html) 'web_title_switcher
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+
   runApp(
     WebTitleSwitcher(
       child: GameWidget(
